166175a4a0b07e17a4d6909b6a4e269a2107faed
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2022-04-18 18:57:18 +0300

    New changes

--- app.py
+++ app.py
@@ -10,14 +10,15 @@ from flask_restful import Api
 
 #Imports for the API
 from src.Root.rootPoint import Root
-from src.Auth.SignUp import SignUp
+from src.Auth.SignUpSession1 import SignUpSession1
 
 app = Flask(__name__)
 api = Api(app)
 
 #Routes
 api.add_resource(Root, '/')
-api.add_resource(SignUp, '/api/signup')
+api.add_resource(SignUpSession1, '/api/signup/1')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
