a85bf3c530ff28a042c32f5f7688b4291e8ad9ab
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2022-08-18 23:37:55 +0300

    Session one test

--- tests/tests.py
+++ tests/tests.py
@@ -5,13 +5,15 @@ import os
 import unittest
 import requests
 import random_name_generator as rng
+import jwt
+from time import time
 
 load_dotenv()
 
 
 class AuthTests(unittest.TestCase):
     url = f"http://{os.environ.get('HOST')}:{os.environ.get('PORT')}"
-
+    first_session_token: str = ""
 
     def test_http_code(self):
         response = requests.get(self.url)
@@ -28,9 +30,17 @@ class AuthTests(unittest.TestCase):
             "email": f'{rng.generate_one().lower().strip().replace(" ", "_")}@gmail.com'
         }
         response = requests.post(url=session1_url, json=body, headers={"lang": "ro"})
-        print(response.text)
+        token: str = json.loads(response.text).get('token')
+        AuthTests.first_session_token = token
         self.assertEqual(response.status_code, 200)
 
+    def test_session_one_token(self):
+        decoded = jwt.decode(self.first_session_token, options={"verify_signature": False})
+        payload = decoded.get('payload')
+        self.assertIsNotNone(payload.get('firstName'))
+        self.assertIsNotNone(payload.get('lastName'))
+        self.assertIsNotNone(payload.get('email'))
+        self.assertGreater(decoded.get('exp'), time())
 
 if __name__ == "__main__":
     unittest.main()
